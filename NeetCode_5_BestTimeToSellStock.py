0119ed13a5bb6d72ba8a74f0cd8c3e9b9c6e3798
Saksham Rai <raisaksham2001@gmail.com>
2023-12-18 14:59:26 -0800

    Update NeetCode_5_BestTimeToSellStock.py

--- NeetCode_5_BestTimeToSellStock.py
+++ NeetCode_5_BestTimeToSellStock.py
@@ -1,1 +1,18 @@
+121. Best Time to Buy and Sell Stock
+Hint: Two - Pointers
+Code: 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0, 1 #Buying, Selling
+        maxProfit = 0
+        
+        #Is this transaction prfofitable?
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else:
+                left = right
+            right += 1
 
+        return maxProfit
