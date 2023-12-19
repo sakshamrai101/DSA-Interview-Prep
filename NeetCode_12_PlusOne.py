92a5d56c2c2a6e99c8589494809534f972f3434d
Saksham Rai <raisaksham2001@gmail.com>
2023-12-18 18:25:53 -0800

    Create NeetCode_12_PlusOne.py

---
+++ NeetCode_12_PlusOne.py
@@ -0,0 +1,23 @@
+66. Plus One
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+My Solution:
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = digits[::-1]
+        one , i = 1, 0
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i += 1
+        return digits[::-1]
