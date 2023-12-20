02cd42443d91857e600e9a9339760df8ab137c5e
Saksham Rai <raisaksham2001@gmail.com>
2023-12-19 19:53:30 -0800

    Create NeetCode_18_ValidParenthesis.py

---
+++ NeetCode_18_ValidParenthesis.py
@@ -0,0 +1,23 @@
+20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {")":"(", "]":"[", "}":"{"}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False 
+            else:
+                stack.append(c)
+        return True if not stack else False 
+        
