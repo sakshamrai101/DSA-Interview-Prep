d87c1739e9a882baa639fd7e846e9314d15ad7cd
Saksham Rai <raisaksham2001@gmail.com>
2023-12-26 18:18:49 -0800

    Create NeetCode_23_ClimbingStairs.py

---
+++ NeetCode_23_ClimbingStairs.py
@@ -0,0 +1,10 @@
+70. Climbing Stairs
+Solution:
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+        for i in range(n - 1):
+            temp = one 
+            one = one + two 
+            two = temp
+        return one 
