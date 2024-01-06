ea012f5516050f941288dda1456d6b966d89ffa5
Saksham Rai <raisaksham2001@gmail.com>
2024-01-06 02:14:04 -0800

    Create NeetCode_32_Kth_Smallest_BST.py

---
+++ NeetCode_32_Kth_Smallest_BST.py
@@ -0,0 +1,16 @@
+230. Kth Smallest Element in a BST
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        n = 0
+        stack = []
+        curr = root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            n += 1
+            if n == k:
+                return curr.val
+            curr = curr.right
