ef0edec1d6c295431e24a26b61bc0b7b9b85490f
Saksham Rai <raisaksham2001@gmail.com>
2024-01-06 01:50:17 -0800

    Create NeetCode_31_Count_Good_Nodes_BST.py

---
+++ NeetCode_31_Count_Good_Nodes_BST.py
@@ -0,0 +1,17 @@
+1448. Count Good Nodes in Binary Tree
+
+My Sol:
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+            
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+        
+        return dfs(root, root.val)
