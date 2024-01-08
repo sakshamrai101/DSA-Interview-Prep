f3fa768c95c23547db3353111f5fd57c6a344044
Saksham Rai <raisaksham2001@gmail.com>
2024-01-07 16:40:33 -0800

    Create NeetCode_36_Right_Side_View_BT.py

---
+++ NeetCode_36_Right_Side_View_BT.py
@@ -0,0 +1,22 @@
+199. Binary Tree Right Side View
+My Solution:
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = collections.deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+
+            for i in range (qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node 
+                    q.append(node.left)
+                    q.append(node.right)
+
+            if rightSide:
+                res.append(rightSide.val)
+        return res
