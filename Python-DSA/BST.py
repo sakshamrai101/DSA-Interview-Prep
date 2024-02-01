7e825d8280e8279431cc48ac8bd27ba044a5ad37
sakshamrai101 <77190532+sakshamrai101@users.noreply.github.com>
2024-02-01 11:15:45 -0800

    Tree Traversal

--- Python-DSA/BST.py
+++ Python-DSA/BST.py
@@ -55,23 +55,78 @@ class BinarySearchTree:
 
         return False 
     
-
     def BFS(self):
-        current_node = self.root
         queue = []
         results = []
-        
+        current_node = self.root
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+    
+
+    def dfs_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
+    
+    def dfs_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+
+        traverse(self.root)
+        return results 
+    
+
+    def dfs_in_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results 
+
+
+
         
 
 
 new_tree = BinarySearchTree()
-new_tree.insert(10)
-new_tree.insert(5)
-new_tree.insert(13)
-new_tree.insert(3)
-new_tree.insert(6)
-new_tree.insert(11)
-new_tree.insert(17)
+new_tree.insert(47)
+new_tree.insert(21)
+new_tree.insert(76)
+new_tree.insert(18)
+new_tree.insert(27)
+new_tree.insert(52)
+new_tree.insert(82)
 
-print(new_tree.contains(5))
+print(new_tree.BFS())
+print(new_tree.dfs_pre_order())
+print(new_tree.dfs_post_order())
+print(new_tree.dfs_in_order())
 
