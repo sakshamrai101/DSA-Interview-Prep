cc0ef53b41793e923d2e13c109cf7a899d17e4f4
sakshamrai101 <77190532+sakshamrai101@users.noreply.github.com>
2024-02-04 20:31:28 -0800

    BST Recursive Methods Added

--- Python-DSA/BST.py
+++ Python-DSA/BST.py
@@ -109,7 +109,75 @@ class BinarySearchTree:
                 traverse(current_node.right)
 
         traverse(self.root)
-        return results 
+        return results
+
+
+
+    def __r_contains(self, current_node, value):
+        if current_node == None:
+            return False
+
+        if value == current_node.value:
+            return True
+
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value) 
+
+    def r_insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)  
+
+    def __r_insert(self, current_node, value):
+        if current_node == None:
+            return Node(value)
+
+        if value == current_node.value:
+            return False
+
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value) 
+
+        return current_node
+    
+
+    def __delete_node(self, current_node, value):
+        if current_node is None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.right is None and current_node.left is None:
+                return None
+            elif current_node.left is None:
+                current_node = current_node.right
+            elif current_node.right is None:
+                current_node = current_node.left 
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+                
+        return current_node
+    
+
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+    
+
+    def delete_node(self, value):
+        self.root = self.__delete_node(self.root, value)
 
 
 
@@ -124,9 +192,8 @@ new_tree.insert(18)
 new_tree.insert(27)
 new_tree.insert(52)
 new_tree.insert(82)
+new_tree.r_insert(65)
 
+new_tree.delete_node(65)
 print(new_tree.BFS())
-print(new_tree.dfs_pre_order())
-print(new_tree.dfs_post_order())
-print(new_tree.dfs_in_order())
 
