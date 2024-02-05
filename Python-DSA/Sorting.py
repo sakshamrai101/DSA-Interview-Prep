12c889a8f0b0f4e3317648f7b090ee2d03b8659c
sakshamrai101 <77190532+sakshamrai101@users.noreply.github.com>
2024-02-04 21:26:11 -0800

    Merge-Sort Added

--- Python-DSA/Sorting.py
+++ Python-DSA/Sorting.py
@@ -56,8 +56,37 @@ def pivot(my_list, pivot_index, end_index):
     return swap_index
 
 
+def merge(list1, list2):
+    combined = []
+    i, j = 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+    return combined
+
+def merge_sort(my_list):
+    if len(my_list) == 1:
+        return my_list
+    mid_index = int(len(my_list)/2)
+    left = merge_sort(my_list[:mid_index])
+    right = merge_sort(my_list[mid_index:])
+
+    return merge(left, right)
+
 
 print(bubble_sort([6,4,9,1,5,15,44,2, 34]))
-print(selection_sort([6,4,9,1,5,15,44,2, 34]))
-print(insertion_sort([6,4,9,1,5,15,44,2, 34]))
-print(quick_sort([6,4,9,1,5,15,44,2, 34], 0, 8))
+print(selection_sort([6,4,9,1,5,15,44,2,34]))
+print(insertion_sort([6,4,9,1,5,15,44,2,34]))
+print(quick_sort([6,4,9,1,5,15,44,2,34], 0, 8))
+
+print(merge_sort([6,4,9,1,5,15,44,2,34]))
