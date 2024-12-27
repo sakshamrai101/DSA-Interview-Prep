d14d420433b5772c170903bcba73952f6a8a0ae5
sakshamrai101 <77190532+sakshamrai101@users.noreply.github.com>
2024-12-27 16:23:37 +0530

    Trie

---
+++ Tries/Prefix_Tree_Trie.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, value, child):
+        self.value = value 
+        self.child = None 
+
+class Trie:
+    def __init__(self) -> None:
+    
+    def insert(self, word: str) -> None:
+    
+    def search(self, word: str) -> bool:
+    
+    def startsWith(self, preFix: str) -> bool:
+
